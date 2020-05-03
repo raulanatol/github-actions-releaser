b36415a48bfae7adabab3ce426d63fab2fca388e
raulanatol <raul@natol.es>
2020-05-03 15:40:15 +0100

    Update

--- src/releaseNotes.ts
+++ src/releaseNotes.ts
@@ -3,7 +3,8 @@ import * as core from '@actions/core';
 export const releaseNotes = (github, context) => {
   const { owner, repo } = context;
 
-  core.debug(`Owner: ${owner} - Repo: ${repo}`);
+  core.debug(`Owner: ${owner} - Repo: ${JSON.stringify(repo)}`);
+  core.debug(`Context: ${JSON.stringify(context)}`);
 
   return '# NEW CHANGES';
 };
